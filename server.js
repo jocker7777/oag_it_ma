5959fe1e5a6e4260bbb681306622cc7682829fd5
Ntdanai1811 <nutdanai1811@gmail.com>
2023-10-11 10:09:47 +0700

    ขึ้นโครงสร้าง
    ขึ้นโครงสร้าง

--- server.js
+++ server.js
@@ -1,86 +1,36 @@
-const express = require("express");
-const mysql = require("mysql2");
+const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
-const port =  5500;
+const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON requests
-app.use(express.json());
-
-// Create a MySQL database connection
+// การเชื่อมต่อกับ MySQL
 const db = mysql.createConnection({
-  host: "172.16.10.151",
-  user: "eticket",
-  password: "p@ssw0rd",
-  database: "eticket",
-});
+    connectionLimit: 10,
+    host: "172.16.10.151",
+    post: 3306,
+    user: "eticket",
+    password: "p@ssw0rd",
+    database: "eticket",
+    insecureAuth : true
+  });
 
 db.connect((err) => {
   if (err) {
-    console.error("Error connecting to MySQL:", err);
-    return;
+    throw err;
   }
-  console.log("Connected to MySQL");
+  console.log('Connected to MySQL');
 });
 
-// Define your API routes here
-app.get("/", (req, res) => {
-  res.send("Welcome to your REST API!");
+// สร้าง API เพื่อดึงข้อมูล
+app.get('/api/oag_office', (req, res) => {
+  const sql = 'SELECT * FROM oag_office LIMIT 10'; // เปลี่ยน mytable เป็นชื่อตารางของคุณ
+  db.query(sql, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
 });
-
-// Start the server
+// เริ่มต้นเซิร์ฟเวอร์
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// Get all todos
-app.get("/user", (req, res) => {
-  //console.log(`Server is running on port ${port}`);
-  db.query("SELECT * FROM oag_inventory", (err, results) => {
-    if (err) {
-      console.error("Error fetching todos:", err);
-      res.status(500).json({ error: "Internal Server Error" });
-      return;
-    }
-    res.json(results);
-  });
-});
-
-// Create a new todo
-// app.post('/todos', (req, res) => {
-//   const { title, completed } = req.body;
-//   db.query('INSERT INTO todos (title, completed) VALUES (?, ?)', [title, completed], (err, result) => {
-//     if (err) {
-//       console.error('Error creating todo:', err);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//       return;
-//     }
-//     res.status(201).json({ id: result.insertId, title, completed });
-//   });
-// });
-
-// Update a todo by ID
-// app.put('/todos/:id', (req, res) => {
-//   const { id } = req.params;
-//   const { title, completed } = req.body;
-//   db.query('UPDATE todos SET title=?, completed=? WHERE id=?', [title, completed, id], (err) => {
-//     if (err) {
-//       console.error('Error updating todo:', err);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//       return;
-//     }
-//     res.status(200).json({ id, title, completed });
-//   });
-// });
-
-// Delete a todo by ID
-// app.delete('/todos/:id', (req, res) => {
-//   const { id } = req.params;
-//   db.query('DELETE FROM todos WHERE id=?', [id], (err) => {
-//     if (err) {
-//       console.error('Error deleting todo:', err);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//       return;
-//     }
-//     res.status(204).send();
-//   });
-// });
