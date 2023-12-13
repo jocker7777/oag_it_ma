438f9bdcf9e51613a484a6c7dc213c95e2851dfb
tanawat.temporary@gmail.com <tanawat.temporary@gmail.com>
2023-12-13 15:53:56 +0700

    wait for connect before start

--- connectdb.js
+++ connectdb.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    connectionLimit: 10,
-    host: "172.16.10.151",
-    port: 3306,
-    user: "eticket",
-    password: "p@ssw0rd",
-    database: "eticket",
-    insecureAuth: true,
-  });
-  
-  db.connect((err) => {
-    if (err) {
-      throw err;
-    }
-    global.globalDB = db;
-    console.log("Connected to MySQL");
-  });
+  connectionLimit: 10,
+  host: "172.16.10.151",
+  port: 3306,
+  user: "eticket",
+  password: "p@ssw0rd",
+  database: "eticket",
+  insecureAuth: true,
+});
 
-module.exports = db;
+db.connect((err) => {
+  if (err) {
+    throw err;
+  }
+  global.globalDB = db;
+  console.log("Connected to MySQL");
+  module.exports = db;
+});
